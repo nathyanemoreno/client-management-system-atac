eb77de684e23ab2871b443d6ea17fe97d173b035
Nathyane Moreno <nathyane.moreno@gmail.com>
2024-08-12 19:17:02 -0300

    feat: add clients/route  to router

--- api/src/router.ts
+++ api/src/router.ts
@@ -4,6 +4,7 @@ import { authSignUpController } from "./controllers/auth/sign-up";
 import { createClientController } from "./controllers/create-client";
 import { listClientController } from "./controllers/list-client";
 import { authMiddleware } from "./middlewares/auth-middleware";
+import { calculateRouteController } from "./modules/calculate-route/clientRoutes";
 
 const router = Router();
 
@@ -14,5 +15,7 @@ router.post("/auth/sign-up", authSignUpController);
 // Client routes
 router.get("/client", [authMiddleware], listClientController);
 router.post("/client", [authMiddleware], createClientController);
+
+router.get("/clients/route", [authMiddleware], calculateRouteController.handle);
 
 export { router };
