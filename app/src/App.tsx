8769e6490386d67aeb2e1ddfd2c67968a041ca69
Nathyane Moreno <nathyane.moreno@gmail.com>
2024-08-12 19:21:12 -0300

    feat(app): add frontend app configuration

---
+++ app/src/App.tsx
@@ -0,0 +1,29 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import store from './application/store';
+import { queryClient } from './infra/config/react-query/queryClient';
+import { AuthProvider } from './presentation/containers/AuthProvider';
+import { router } from './routes/router';
+import { theme } from './theme';
+import { GlobalStyle } from './theme/style/globalStyle';
+import './theme/style/reset.css';
+import 'leaflet/dist/leaflet.css';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </QueryClientProvider>
+      </Provider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
