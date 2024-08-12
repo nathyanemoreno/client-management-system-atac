8d1c3e97f56204352aa7e8b99f9773cc726c52c3
Nathyane Moreno <nathyane.moreno@gmail.com>
2024-08-12 19:17:20 -0300

    feat: add sign-in-with-token  to router

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authSignInController } from "./controllers/auth/sign-in";
+import { authSignInWithTokenController } from "./controllers/auth/sign-in-with-token";
 import { authSignUpController } from "./controllers/auth/sign-up";
 import { createClientController } from "./controllers/create-client";
 import { listClientController } from "./controllers/list-client";
@@ -11,6 +12,7 @@ const router = Router();
 // Authentication routes
 router.post("/auth/sign-in", authSignInController);
 router.post("/auth/sign-up", authSignUpController);
+router.post("/auth/token", authSignInWithTokenController);
 
 // Client routes
 router.get("/client", [authMiddleware], listClientController);
